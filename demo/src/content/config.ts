fe277f3f51c82ef1cba513e10a7b0490a6cb7720
Julian Cataldo <contact@juliancataldo.com>
2023-10-24 11:39:38 +0200

    docs: implement remark + library usage for demo

--- demo/src/content/config.ts
+++ demo/src/content/config.ts
@@ -1,4 +1,5 @@
 import { defineCollection, z } from 'astro:content';
+import { astroZodCollectionsToJsonSchemas } from 'astro-zod-to-json-schema';
 
 const blog = defineCollection({
 	// Type-check frontmatter using a schema
@@ -13,3 +14,5 @@ const blog = defineCollection({
 });
 
 export const collections = { blog };
+
+await astroZodCollectionsToJsonSchemas(collections);
